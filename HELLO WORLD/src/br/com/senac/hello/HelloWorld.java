8fee737cd31b5ca73f5b4fd4a74262a186f81557
Victor Henrique Ranalli Barbosa <vhvictor978@gmail.com>
2017-08-03 00:03:46 -0300

    Add files via upload

---
+++ HELLO WORLD/src/br/com/senac/hello/HelloWorld.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.senac.hello;
+
+/**
+ * Projeto Hello World
+ *
+ * @author victor Hanrique Ranalli Barbosa BY:zeradordejogos
+ */
+/* Classe Principal*/
+public class HelloWorld {
+
+    //metodo principal
+    public static void main(String[] args) {
+        //escrevendo no terminal do sistema (windows, linus e outros)
+        System.out.println("HelloWorld");
+    }
+}
